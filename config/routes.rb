223ad98699da501fd79f0bc4c4cc4c0e737b750d
Blake Bigelow <blbigelow@gmail.com>
2009-04-11 16:09:31 -0600

    added menu plugin and adding more layout

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :documents
 
   map.resources :discussions
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
